33ae2e7d95969d8825fb7a0faa869852a0a37db9
Cyril <cyril.russe@hotmail.com>
2020-03-04 09:30:15 +0100

    modifications initiales

--- prog-5.py
+++ prog-5.py
@@ -23,57 +23,97 @@ ROUGE = (255, 0, 0)
 variable_memorisee = 0
 
 
-def dessiner_arduino(sortie_arduino, sortie_CD4511, sortie_bouton):
+def dessiner_arduino(sortie_arduino, sortie_CD4511, sortie_CD4028, sortie_bouton):
     fenetre.blit(image_arduino, pos_arduino)
     fenetre.blit(image_CD4511, pos_CD4511)
     fenetre.blit(image_bouton, pos_bouton)
+    fenetre.blit(image_CD4028, pos_CD4028)
 
 
-    off_ard = 194
+    for j in range(0, 2):
+        if j == 0:
+            off_ard = 285
+            off_cd = 15
+            pos_carte = pos_CD4511
+            r = range(0, 4)
+
+        if j == 1:
+            off_ard = 194
+            off_cd = 91
+            pos_carte = pos_CD4028
+            r = range(4, 8)
+
+        for i in r:
+            if sortie_arduino[i] == 0:
+                couleur = NOIR
+            else:
+                couleur = ROUGE
+
+            pygame.draw.line(fenetre, couleur, (pos_arduino[0] + 280, pos_arduino[1] + off_ard),
+                            (pos_carte[0] + 7, pos_carte[1] + off_cd), 5)
+            off_ard = off_ard + 14
+            off_cd = off_cd + 19
+
+
+
     off_cd = 15
-    for i in range(0, 4):
-        if sortie_arduino[i] == 0:
-            couleur = NOIR
-        else:
-            couleur = ROUGE
-
-        pygame.draw.line(fenetre, couleur, (pos_arduino[0] + 280, pos_arduino[1] + off_ard),
-                        (pos_CD4511[0] + 7, pos_CD4511[1] + off_cd), 5)
-        off_ard = off_ard + 14
-        off_cd = off_cd + 19
-
-
-    off_cd = 15
-    off_aff = 27
+    off_aff = 5
+    i = 0
     for i in range(0, 7):
         if sortie_CD4511[i] == 0:
             couleur = NOIR
         else:
             couleur = ROUGE
-        pygame.draw.line(fenetre, couleur, (pos_afficheur[0], pos_afficheur[1] + off_aff),
+        pygame.draw.line(fenetre, couleur, (pos_afficheur[0] + 591, pos_afficheur[1] + off_aff),
                         (pos_CD4511[0] + 102, pos_CD4511[1] + off_cd), 5)
         off_aff = off_aff + 19
         off_cd = off_cd + 19
 
-    connexion_bouton(sortie_bouton)
 
-def dessiner_afficheur(sortie_CD4511):
-    positions_barres = [[32, 14], [89, 20], [87, 88], [28, 150],
-                        [17, 88], [19, 20], [30, 82]]
-    fenetre.blit(image_afficheur, pos_afficheur)
+    if sortie_bouton == 0:
+        couleur = NOIR
+    else:
+        couleur = ROUGE
+    pygame.draw.line(fenetre, couleur, (pos_arduino[0] + 279, pos_arduino[1] + 353),
+                        (pos_bouton[0] + 13, pos_bouton[1] + 13), 5)
 
     i = 0
-    for barre in positions_barres:
-        if sortie_CD4511[i] == 0:
-            i = i + 1
-            continue
-        x_b = pos_afficheur[0] + int(round(barre[0]*(image_afficheur.get_width()/133)))
-        y_b = pos_afficheur[1] + int(round(barre[1]*(image_afficheur.get_height()/192)))
-        if i == 0 or i == 3 or i == 6:
-            fenetre.blit(barre_horizontale, (x_b, y_b))
+    off_cd = (102, 111)
+    off_aff = 44
+    for i in range(0, 6):
+        if sortie_CD4028[i] == 0:
+            couleur = NOIR
         else:
-            fenetre.blit(barre_verticale, (x_b, y_b))
-        i = i + 1
+            couleur = ROUGE
+        pygame.draw.line(fenetre, couleur, (pos_CD4028[0] + off_cd[0], pos_CD4028[1] + off_cd[1]),
+                        (pos_afficheur[0] + off_aff, pos_CD4028[1] + off_cd[1]), 5)
+
+        pygame.draw.line(fenetre, couleur, (pos_afficheur[0] + off_aff, pos_afficheur[1]),
+                        (pos_afficheur[0] + off_aff, pos_CD4028[1] + off_cd[1] - 2), 5)
+        off_cd = (off_cd[0], off_cd[1] - 20)
+        off_aff = off_aff + 101
+
+
+
+def dessiner_afficheur(sortie_CD4511, sortie_CD4028):
+    positions_barres = [[32, 14], [89, 20], [87, 88], [28, 150],
+                        [17, 88], [19, 20], [30, 82]]
+
+    for j in range(0, 6):
+        fenetre.blit(image_afficheur_s, (pos_afficheur[0] + j*101, pos_afficheur[1]))
+        if sortie_CD4028[j] == 1:
+            i = 0
+            for barre in positions_barres:
+                if sortie_CD4511[i] == 0:
+                    i = i + 1
+                    continue
+                x_b = j*101 + pos_afficheur[0] + int(round(barre[0]*(image_afficheur_s.get_width()/133)))
+                y_b = pos_afficheur[1] + int(round(barre[1]*(image_afficheur_s.get_height()/192)))
+                if i == 0 or i == 3 or i == 6:
+                    fenetre.blit(barre_horizontale_s, (x_b, y_b))
+                else:
+                    fenetre.blit(barre_verticale_s, (x_b, y_b))
+                i = i + 1
     return
 
 def dessiner_cercle():
@@ -144,11 +184,12 @@ def connexion_bouton(sortie_bouton):
 dimensions_fenetre = (1100, 600)  # en pixels
 images_par_seconde = 25
 
-pos_arduino = (65, 84)
-pos_CD4511 = (537, 263)
-pos_afficheur = (818, 251)
-pos_bouton = (537, 486)
-pos_centre_bouton = (589, 521)
+pos_arduino = (0, 70)
+pos_CD4511 = (333, 340)
+pos_CD4028 = (333, 128)
+pos_afficheur = (500, 350)
+pos_bouton = (333, 524)
+pos_centre_bouton = (pos_bouton[0] + 51, pos_bouton[1] + 34)
 rayon_bouton = 18
 pin_arduino = (pos_arduino[0] + 279, pos_arduino[1] + 353)
 pin_bouton = (pos_bouton[0] + 13, pos_bouton[1] + 13)
@@ -208,10 +249,9 @@ while True:
 
     fenetre.fill(couleur_fond)
 
-    sortie_CD4511 = composant_CD4511(sortie_memorisee())
-    dessiner_afficheur(sortie_CD4511)
-    dessiner_arduino(sortie_memorisee(), sortie_CD4511, sortie_bouton)
-    dessiner_cercle()
+    dessiner_arduino(np.zeros(8, dtype=int), np.zeros(7, dtype=int),
+                 np.zeros(6, dtype=int), 0)
+    dessiner_afficheur(np.zeros(7, dtype=int), np.zeros(6, dtype=int))
 
     pygame.display.flip()
     horloge.tick(images_par_seconde)
