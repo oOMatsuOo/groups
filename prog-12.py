b9612ad67161ac756f51c57c2ca976785823666f
Cyril Russe <cyril.russe@student.uliege.be>
2020-05-12 23:53:37 +0200

    FIN prog-12

--- prog-12.py
+++ prog-12.py
@@ -18,6 +18,17 @@ GRIS = (115, 115, 115)
 angle_moteur = math.pi/4
 compteur = 0
 courant = 0
+vitesse_moteur = 0
+tension_moteur = 0
+circuit_est_ouvert = True
+
+K = 1000
+R = 0.02
+L = 0.06
+B = 0.5
+J = 1
+c = 0.2
+Rm = 10
 
 # Paramètres
 
@@ -31,6 +42,44 @@ def position_moteur():
 
     return (int(x), int(y))
 
+def maj_compteur_courant():
+    global tension_moteur
+
+    if not circuit_est_ouvert:
+        tension_moteur = 10
+        angle = math.fmod(angle_moteur, 2*math.pi)
+        if angle>=math.pi/2 and angle<=3*math.pi/2:
+            tension_moteur *=-1
+    else:
+        tension_moteur = 0
+
+def maj_moteur(t):
+    global vitesse_moteur, angle_moteur, temps_prec, tension_moteur, courant
+
+    E = 2*K*R*L*B*vitesse_moteur*math.cos(angle_moteur)
+
+    if circuit_est_ouvert:
+        courant = 0
+        tension_moteur = E
+    else:
+        courant = 10-E/Rm
+        angle = math.fmod(angle_moteur, 2*math.pi)
+        if angle>=math.pi/2 and angle<=3*math.pi/2:
+            courant *= -1
+
+    tho_frottement = -c*vitesse_moteur
+    tho = 2*K*R*L*courant*B*math.cos(angle_moteur) + tho_frottement
+
+    alpha = tho/J
+
+    delta_t = t - temps_prec
+
+    vitesse_moteur += alpha*delta_t
+    angle_moteur += vitesse_moteur*delta_t
+
+    temps_prec = t
+
+
 def affichage_moteur():
     position_repere = position_moteur()
 
@@ -39,23 +88,17 @@ def affichage_moteur():
     pygame.draw.circle(fenetre, BLEUCLAIR, (dimensions_fenetre[0]//2, dimensions_fenetre[1]//2), dimensions_fenetre[1]//5, 0)
     pygame.draw.circle(fenetre, GRIS, (dimensions_fenetre[0]//2, dimensions_fenetre[1]//2), dimensions_fenetre[1]//6, 0)
     pygame.draw.circle(fenetre, NOIR, position_repere, 8, 0)
-
-def maj_compteur_courant():
-    global compteur, courant
-
-    if compteur!=0:
-        compteur -= 1
-        courant = 1
-    else:
-        courant = 0
         
-def affichage_courant():
+def affichage_tableau_bord():
     police = pygame.font.SysFont("monospace", 20)
 
     texte_courant = "Courant : {0:.2f} A".format(courant)
+    texte_tension = "Tension : {0:.2f} V".format(tension_moteur)
 
     image = police.render(texte_courant, True, NOIR)
     fenetre.blit(image, (50, 50))
+    image = police.render(texte_tension, True, NOIR)
+    fenetre.blit(image, (50, 75))
 
 
 
@@ -71,22 +114,32 @@ fenetre.fill(couleur_fond)
 
 pygame.key.set_repeat(10, 10)
 
+temps_precedent = 0
+temps_maintenant = pygame.time.get_ticks()
+temps_prec = temps_maintenant/1000
+
+
 while True:
     fenetre.fill(couleur_fond)
+    temps_maintenant = pygame.time.get_ticks()
 
     maj_compteur_courant()
-
+    for t in range(temps_precedent, temps_maintenant - 1, 1):
+        maj_moteur(t/1000)
+    circuit_est_ouvert = True
     for evenement in pygame.event.get():
         if evenement.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         elif evenement.type == pygame.KEYDOWN:
             if evenement.key == pygame.K_SPACE:
-                compteur = 5
+                circuit_est_ouvert = False
     
 
     affichage_moteur()
-    affichage_courant()
+    affichage_tableau_bord()
+
+    temps_precedent = temps_maintenant
 
     pygame.display.flip()
     horloge.tick(images_par_seconde)
